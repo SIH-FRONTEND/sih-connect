1869473f71388ddd54cdb634fbfa83ffa6626cf5
unknown <Ediana-Ekanem>
2022-12-14 18:27:31 +0100

    refactored PropRecieverPage

--- src/components/available-techies/PropReceiver.jsx
+++ src/components/available-techies/PropReceiver.jsx
@@ -1,34 +1,37 @@
 import React, { useEffect, useState } from "react";
 import UserProp from "./UserProp";
-import data from "./Data";
+import useFetch from "../../hooks/useFetch";
+
 const url = "https://starthubconnect.adaptable.app/user/latest-members";
 
 const PropReceiver = () => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [Error, setError] = useState("");
+  const { data, isPending, error } = useFetch(url);
+  // const [users, setUsers] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [Error, setError] = useState("");
 
-  useEffect(() => {
-    const getLatestMembers = async () => {
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(users);
-      setUsers(data);
-      setIsLoading(false);
-    };
+  // useEffect(() => {
+  //   const getLatestMembers = async () => {
+  //     setIsLoading(true);
+  //     const response = await fetch(url);
+  //     const data = await response.json();
+  //     console.log(users);
+  //     setUsers(data);
+  //     setIsLoading(false);
+  //   };
 
-    try {
-      getLatestMembers();
-    } catch (error) {
-      setError(error);
-      setIsLoading(true);
-      console.log(error);
-    }
-  }, []);
+  //   try {
+  //     getLatestMembers();
+  //   } catch (error) {
+  //     setError(error);
+  //     setIsLoading(false);
+  //     console.log(error);
+  //   }
+  // }, []);
 
-  if (isLoading) {
-    return <div>Loading ....</div>;
-  }
+  // if (isLoading) {
+  //   return <div>Loading ....</div>;
+  // }
 
   return (
     <div>
@@ -37,11 +40,15 @@ const PropReceiver = () => {
           <h2 className=" flex  justify-center text-center items-center font-[500] text-[40px] font-[Raleway]">
             Avialable techies
           </h2>
-          <div className="flex flex-wrap justify-center items-center py-5  gap-y-8 gap-x-0">
-            {users.items.map((item) => {
-              return <UserProp key={item.id} {...item} />;
-            })}
-          </div>
+          {data && !isPending && !error && (
+            <div className="flex flex-wrap justify-center items-center py-5  gap-y-8 gap-x-0">
+              {data.items.map((item) => {
+                return <UserProp key={item.id} {...item} />;
+              })}
+            </div>
+          )}
+          {isPending && !error && !data && <h1>Loading...</h1>}
+          {error && !isPending && !data && <h1>Error Loading Data</h1>}
 
           <button className="text-[#687eb6]  border-2 border-[#687eb6]   px-8 py-1 rounded  hover:bg-[#687eb6] hover:border-white hover:text-white">
             See more
