1bb11b97b3d747608cd83686c7083ff1118645d9
Mfoniso <ukpabiomfoniso@gmail.com>
2022-12-16 22:08:44 +0100

    Minimal Local Changes

--- src/pages/DeveloperProfile.jsx
+++ src/pages/DeveloperProfile.jsx
@@ -1,6 +1,29 @@
+import useFetch from "../hooks/useFetch";
+
 const DeveloperProfile = () => {
+  const { data, isPending, error } = useFetch(
+    "https://starthubconnect.adaptable.app/user/top-members"
+  );
+
+  if (data) {
+    console.log(data);
+  }
+
   return (
     <div>
+      {!isPending && !data && error && (
+        <h1>Could not fetch data : {error.message}</h1>
+      )}
+      {isPending && !error && !data && (
+        <h1 className="text-red-500">Loading...</h1>
+      )}
+      {data && !isPending && !error && (
+        <div>
+          {data.items.map((each, id) => (
+            <h1 key={id}>{each.firstname}</h1>
+          ))}
+        </div>
+      )}
       <h1>DeveloperProfile</h1>
     </div>
   );
