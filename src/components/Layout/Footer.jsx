177febd53330947142317de130b8ba6533535250
AirZED <regnalukpabio@gmail.com>
2023-01-29 06:06:31 +0000

    more commits on home

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -6,21 +6,31 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className=" bg-[#10328C] text-white flex flex-nowrap justify-between p-[1em] xl2:p-[4em] lg:items-center h-auto py-[1em] xl2:h-[14em] xl2:[&>*>*]:text-[1.1em] w-full">
-      <div className="flex items-center flex-col [&>*]:text-[0.5em]">
-        <img src={footLogo} alt="footlogo" className="h-[3em] xl2:h-[4em]" />
-        <p>Copyright &copy; 2022</p>
+    <footer className=" bg-[#10328C] text-white flex flex-nowrap justify-between p-[1em] xl2:p-[4em] lg:items-center h-auto py-[1em] xl2:h-[14em] w-full">
+      <div className="flex items-center flex-col ">
+        <img src={footLogo} alt="footlogo" className="h-[3em] xl2:h-[6em]" />
+        <p className="xl2:text-[1.2em] text-[0.7em]">Copyright &copy; 2022</p>
       </div>
 
-      <div className="flex flex-col items-start xl3:flex-row xl2:gap-[3em] [&>*]:text-[0.7em] ">
-        <Link to="/supportUs ">Terms & Conditions</Link>
-        <Link to="/">Become A Member</Link>
-        <Link to="/">About Us</Link>
+      <div className="flex flex-col items-start xl3:flex-row xl3:gap-[3em] ">
+        <Link to="/supportUs" className="xl2:text-[1.2em] text-[0.7em]">
+          Terms & Conditions
+        </Link>
+        <Link to="/" className="xl2:text-[1.2em] text-[0.7em]">
+          Become A Member
+        </Link>
+        <Link to="/" className="xl2:text-[1.2em] text-[0.7em]">
+          About Us
+        </Link>
       </div>
 
-      <div className="flex flex-col items-start xl3:flex-row xl2:gap-[3em] [&>*]:text-[0.7em]">
-        <Link to="/">Support Us</Link>
-        <Link to="/">Privacy Policy</Link>
+      <div className="flex flex-col items-start xl3:flex-row xl3:gap-[3em]">
+        <Link to="/" className="xl2:text-[1.2em] text-[0.7em]">
+          Support Us
+        </Link>
+        <Link to="/" className="xl2:text-[1.2em] text-[0.7em]">
+          Privacy Policy
+        </Link>
       </div>
     </footer>
   );
